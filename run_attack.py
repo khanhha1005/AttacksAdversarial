2e7a39a4f3b5ad81183764fa58a73ebc77323e0e
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2023-07-13 18:54:47 +0700

    commit

--- run_attack.py
+++ run_attack.py
@@ -63,7 +63,6 @@ if __name__ == "__main__":
                 'img_num':img_num,
                 'epsilon':-1,
                 'img_size':image_labels.img_shape[0]*image_labels.img_shape[1],
-                'truth_iou_noise_avg':-1,
                 'ssim_avg':-1}
             epsilon = epsilon_start_value
             while (epsilon < max_epsilon_value):
@@ -80,7 +79,7 @@ if __name__ == "__main__":
                     continue
                 attack_success_count = 0
                 attack_fail_count = 0
-                truth_iou_noise_avg = 0
+                # truth_iou_noise_avg = 0
                 for img_pair in image_noise_pairs:
                     attacked_img = img_pair.img
                     noise_img = img_pair.noise
@@ -102,7 +101,7 @@ if __name__ == "__main__":
                         + '% of the time with epsilon: ' + str(epsilon))
                     tunnel_dict['successful_attack'] += 1
                     performance_dict['epsilon'] = epsilon
-                    performance_dict['truth_iou_noise_avg'] = truth_iou_noise_avg
+                    # performance_dict['truth_iou_noise_avg'] = truth_iou_noise_avg
                     img = Image.open(image_labels.img_path)
                     img = np.array(img)
                     utils.draw_img_noise_pair_arrays(image_noise_pairs, epsilon, img_num, output_dir)
@@ -122,9 +121,8 @@ if __name__ == "__main__":
                 print('Noise attack failed')
                 attacks.create_noisy_image(img_path,output_dir)
                 print("Add Noise to the whole image")
-
                 performance_dict['epsilon'] = -1
-                performance_dict['truth_iou_noise_avg'] = -1
+                # performance_dict['truth_iou_noise_avg'] = -1
                 performance_dict['ssim_avg'] = -1
                 tunnel_dict['fail_count'] += 1
             performance_list.append(performance_dict)
